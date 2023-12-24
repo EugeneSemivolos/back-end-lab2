86a176b6cdc233e2d46e02484c342eabfdced80b
EugeneSemivolos <semivolos.evgen1@gmail.com>
2023-12-24 20:40:58 +0200

    implemended CRUD for /record

--- src/views.py
+++ src/views.py
@@ -5,6 +5,7 @@ import datetime
 
 users = {}
 categories = {}
+records = {}
 
 @app.get("/user/<user_id>")
 def user_get(user_id):
@@ -59,6 +60,48 @@ def category_delete(category_id):
         return "", 204
     else:
         return "Category not found", 404
+
+
+@app.get("/record")
+def get_records():
+    user_id = request.args.get("user_id")
+    category_id = request.args.get("category_id")
+
+    if user_id is None and category_id is None:
+        return "Missing parameters", 400
+
+    filtered_records = []
+    for record in records.values():
+        if (user_id is None or record.get("user_id") == user_id) and (category_id is None or record.get("category_id") == category_id):
+            filtered_records.append(record)
+
+    return jsonify(filtered_records)
+
+@app.post("/record")
+def create_record():
+    user_id = request.args.get("user_id")
+    category_id = request.args.get("category_id")
+    record_id = uuid.uuid4().hex
+    created_at = datetime.datetime.now().isoformat()
+    amount = request.args.get("amount")
+
+    record = {
+        "id": record_id,
+        "user_id":user_id,
+        "category_id":category_id,
+        "created_at": created_at,
+        "amount": amount
+    }
+    records[record_id] = record
+    return record
+
+@app.delete("/record/<record_id>")
+def record_delete(record_id):
+    if record_id in records:
+        del records[record_id]
+        return "", 204
+    else:
+        return "Record not found", 404
 
 if __name__ == "__main__":
     app.run()
